054f6a5dfdabbd9e4ee7075ae62aa6de4eddfd21
Krishnan Kannan <kxkannan@gmail.com>
2011-03-02 22:17:59 -0700

    changed the method name

--- problem_1.rb
+++ problem_1.rb
@@ -4,9 +4,9 @@
 #Find the sum of all the multiples of 3 or 5 below 1000.
 
 class Problem1
-  def multiples_of_3_or_5
+  def sum_of_multiples_of_3_or_5
     (1..999).inject(0) { |sum, n| (n%3 == 0 || n%5 == 0) ? sum + n : sum }
   end
 end
 
-puts Problem1.new.multiples_of_3_or_5
+puts Problem1.new.sum_of_multiples_of_3_or_5
